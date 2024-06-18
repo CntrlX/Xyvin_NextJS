e3c737e333285a85aeeb954ad64565fc3f4ce420
Abhijith B <babhijith58@gmail.com>
2024-06-18 14:30:31 +0000

    updated carousel

--- src/components/hero/HeroHomeSix.tsx
+++ src/components/hero/HeroHomeSix.tsx
@@ -15,10 +15,13 @@ const HeroCarousel: React.FC = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        minHeight: 'calc(100vh - 300px)', // Calculate height with a gap of 300px from all sides
-        padding: '0 300px', // Add padding of 300px from all sides
+        minHeight: 'calc(60vh - 300px)', // Calculate height with a gap of 300px from all sides
+        padding: '0 100px', // Add padding of 300px from all sides
         boxSizing: 'border-box', // Include padding in the total width/height
-        borderRadius:'20px',
+        borderRadius:'200px',
+        minWidth:'398px',
+        background:'#ffgf',
+        marginTop:'300px',
       }}
     >
       <Swiper
@@ -35,19 +38,43 @@ const HeroCarousel: React.FC = () => {
         modules={[Autoplay, Pagination, Navigation]}
         style={{ width: '100%', height: '100%' }}
       >
-      <SwiperSlide className="slide" style={{ backgroundImage: 'url(https://plus.unsplash.com/premium_photo-1677653128215-43b4e3732d17?q=80&w=1382&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)' }}>
+      <SwiperSlide className="slide"  style={{
+            backgroundImage:
+              'url(https://plus.unsplash.com/premium_photo-1677653128215-43b4e3732d17?q=80&w=1382&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
+            backgroundSize: 'cover', // Ensure the background image covers the entire slide
+            backgroundPosition: 'center', // Center the background image
+            minHeight: '300px', // Set the minimum height of the slide
+            borderRadius: '20px', // Border radius of 20px
+          }}
+        >
         <div className="slideContent">
           <h2>Slide 1 Title</h2>
           <p>Slide 1 Description</p>
         </div>
       </SwiperSlide>
-      <SwiperSlide className="slide" style={{ backgroundImage: 'url(https://plus.unsplash.com/premium_photo-1664302274909-2971dc5375d1?dpr=1&w=306&auto=format&fit=crop&q=60&crop=entropy&cs=tinysrgb&fm=jpg&ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTB8fGFkZHJlc3N8ZW58MHwwfHx8MTcxODY1MzEyNHwx&ixlib=rb-4.0.3)' }}>
+      <SwiperSlide className="slide"  style={{
+            backgroundImage:
+              'url(https://plus.unsplash.com/premium_photo-1677653128215-43b4e3732d17?q=80&w=1382&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
+            backgroundSize: 'cover', // Ensure the background image covers the entire slide
+            backgroundPosition: 'center', // Center the background image
+            minHeight: '300px', // Set the minimum height of the slide
+            borderRadius: '20px', // Border radius of 20px
+          }}
+         >
         <div className="slideContent">
           <h2>Slide 2 Title</h2>
           <p>Slide 2 Description</p>
         </div>
       </SwiperSlide>
-      <SwiperSlide className="slide" style={{ backgroundImage: 'url(/assets/img/hero_img_1.jpg)' }}>
+      <SwiperSlide className="slide"  style={{
+            backgroundImage:
+              'url(https://plus.unsplash.com/premium_photo-1677653128215-43b4e3732d17?q=80&w=1382&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
+            backgroundSize: 'cover', // Ensure the background image covers the entire slide
+            backgroundPosition: 'center', // Center the background image
+            minHeight: '300px', // Set the minimum height of the slide
+            borderRadius: '20px', // Border radius of 20px
+          }}
+        >
         <div className="slideContent">
           <h2>Slide 3 Title</h2>
           <p>Slide 3 Description</p>
